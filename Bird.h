f03d2f68233a34d92d4face0d6fba3738b30709f
21020716-VuTrongHieu <100186254+21020716-VuTrongHieu@users.noreply.github.com>
2022-05-18 00:35:03 +0700

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -11,17 +11,10 @@ public:
     BirdObject();
     ~BirdObject();
 
-    enum WalkType
-    {
-        WALK_UP = 0,
-        WALK_DOWN = 1,
-    };
-
     bool LoadImg(string path, SDL_Renderer* render);
     void Show(SDL_Renderer* des);
     void HandelInputAction(SDL_Event events, SDL_Renderer* render);
     void set_clips();
-
     void DoPlayer();
 
     SDL_Rect* current_clip;
@@ -35,23 +28,11 @@ public:
 private:
     float x_val_;
     float y_val_;   // thay doi cua nhan vat tren man hinh
-
     float x_pos_; // thong tin vi tri xuat nhan vat ra man hinh
-    //float y_pos_;
-
-    //float speed_ ;  // toc do roi tu do
     int width_frame_;
     int height_frame_;  // thong tin tung buc anh nhan vat
-
     SDL_Rect frame_clip_[3];    // luu 3 buc anh lam animation
-    //Input input_type_;
     int frame_;  // luu chi so cua buc anh nhan vat
-    int status_;  // luu trang thai nhan vat de quyet dinh dung anh nao cua nhan vat
-
-    //= &frame_clip_[frame_]; // 1 ty thay bang frame_
-
-
-    //= {rect_.x, rect_.y, width_frame_, height_frame_}; // thong tin anh xuat ra man hinh
 } ;
 
 
